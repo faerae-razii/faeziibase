abe2a31dfd66cc87c5a79b3a527ffa2ae3df81b9
Hendra Faerae <hendra.faerae@gmail.com>
2022-11-23 23:33:23 +0700

    Add test BaseViewController

---
+++ Sources/faeziibase/Controllers/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Faerae on 23/11/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    // MARK: - Properties
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    }
+}
