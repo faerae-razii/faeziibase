69ba298a91875b80a2dd6ab43c9306fd53b3c3de
Hendra Faerae <hendra.faerae@gmail.com>
2022-11-24 01:31:25 +0700

    Revert "Update package name"
    This reverts commit 77b95468dc0965714ab003918816ccb27601bc2f.

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "FaeZiiBase",
+    name: "faeziibase",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "FaeZiiBase",
+            name: "faeziibase",
             targets: ["faeziibase"]),
     ],
     dependencies: [
